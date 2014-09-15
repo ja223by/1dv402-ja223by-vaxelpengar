28f364a4ec6b0da62b0dc2903bf11c94e5c763ad
ja223by <johanalm94@outlook.com>
2014-09-15 14:26:11 +0200

    First Done

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vaxelpengar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarera variablar och skriv ut summor
+            double totalt;
+            double öresavrunging;
+            int attbetala;
+            int kontant;
+            int tillbaka;
+
+            totalt = 371.38;
+            öresavrunging = 0.38;
+            attbetala = 371;
+            kontant = 1000;
+            tillbaka = 629;
+
+            kontant + attbetala = tillbaka;
+
+
+            Console.WriteLine("Du får ---- mycket pengar tillbaka!");
+
+        }
+    }
+}
